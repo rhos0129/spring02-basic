0261e1bdcb4a082fae29af284aca9e6481a4e378
rhos0129 <rhos0129@gmail.com>
2023-12-05 22:13:35 +0900

    S09 스코프와 Provider

--- src/main/java/com/example/springbasic/web/LogDemoController.java
+++ src/main/java/com/example/springbasic/web/LogDemoController.java
@@ -3,6 +3,7 @@ package com.example.springbasic.web;
 import com.example.springbasic.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,14 +13,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService  logDemoService;
-    private final MyLogger myLogger;
 
-    // 에러 발생
-    // request가 없으므로 MyLogger를 DI할 수 없음. 즉, DL 필요
+//    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider; // 해결방안1 ObjectProvider<> 이용
+
+    // 정상작동
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
