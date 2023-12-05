66d6c633a72ac0b7d7911c80690867df8d8cc0f9
rhos0129 <rhos0129@gmail.com>
2023-12-05 14:56:18 +0900

    S07 조회 빈이 2개 이상 - 문제

--- src/main/java/com/example/springbasic/discount/FixDiscountPolicy.java
+++ src/main/java/com/example/springbasic/discount/FixDiscountPolicy.java
@@ -3,7 +3,9 @@ package com.example.springbasic.discount;
 
 import com.example.springbasic.member.Grade;
 import com.example.springbasic.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000; // 1000원 할인
