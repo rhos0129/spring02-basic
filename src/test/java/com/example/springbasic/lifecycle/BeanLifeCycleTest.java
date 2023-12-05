724e8909774e023bf2f590adbb4c6561e1f1d712
rhos0129 <rhos0129@gmail.com>
2023-12-05 19:21:53 +0900

    S08 빈 생명주기 콜백 시작

---
+++ src/test/java/com/example/springbasic/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,36 @@
+package com.example.springbasic.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+
+        // 결과 : 생성자에서도, 서비스 시작시에도 url=null
+        // 생성자 호출, url = null
+        // connect : null
+        // call : null message = 초기화 연결 메시지
+
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+
+    }
+
+}
