11e8cbdac494d65cdbdea1cad990545e21b4a2c0
rhos0129 <rhos0129@gmail.com>
2023-12-05 21:41:06 +0900

    S09 프로토타입 스코프

---
+++ src/test/java/com/example/springbasic/scope/SingletonTest.java
@@ -0,0 +1,45 @@
+package com.example.springbasic.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind(){
+
+        // 스프링 컨테이너 생성 시에 init() 호출
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+
+        assertThat(singletonBean1).isSameAs(singletonBean2); // 항상 같은 인스턴스
+
+        ac.close(); // destroy()도 정상 호출
+
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+
+        @PostConstruct
+        public void init(){
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("SingletonBean.close");
+        }
+
+    }
+}
