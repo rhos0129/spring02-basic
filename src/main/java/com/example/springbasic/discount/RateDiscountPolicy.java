f5dcf9e5175006d7b374a0a7ad5713365fddbdad
rhos0129 <rhos0129@gmail.com>
2023-12-05 15:10:03 +0900

    S07 @Autowired 필드 명, @qualifier, @primary

--- src/main/java/com/example/springbasic/discount/RateDiscountPolicy.java
+++ src/main/java/com/example/springbasic/discount/RateDiscountPolicy.java
@@ -2,9 +2,13 @@ package com.example.springbasic.discount;
 
 import com.example.springbasic.member.Grade;
 import com.example.springbasic.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
+//@Qualifier("mainDiscountPolicy")
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
--- src/main/java/com/example/springbasic/order/OrderServiceImpl.java
+++ src/main/java/com/example/springbasic/order/OrderServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.springbasic.discount.DiscountPolicy;
 import com.example.springbasic.member.Member;
 import com.example.springbasic.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +14,20 @@ public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+//    // @Autowired 필드명 매칭
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = rateDiscountPolicy;
+//    }
+
+//    // @Qualifier 사용
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     // 테스트 용도
     public MemberRepository getMemberRepository() {
